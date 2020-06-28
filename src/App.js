884d0c6908c4cb4654f28502c07a4dcd275d9db4
Muhammad Hammad <hammadarshad834@gmail.com>
2020-06-28 10:51:57 +0500

    Hello world!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          New edited App.js file
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name = '<h1>Hello World! from Hammad</h1>'
+  return name
 }
 
 export default App;
